3fc2b51234cbe440fc1c17364fe7b4c3b80aa150
Paul Lytvyn <36439227+PaulLitvin@users.noreply.github.com>
2018-10-09 21:24:01 +0300

    v1.0

---
+++ L4T2/js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+function getFriendlyNumbers(start, end) {
+	
+	if (typeof (start) != "number" || typeof (end) != "number" ||
+		start > end || start < 0 || end < 0 || parseInt(start) != start || parseInt(end) != end) {
+		console.log(false);
+		return false;
+	}
+	//Здесь находим все делители для диапазона чисел и помещяем их в массив
+	let arr = [];
+	let finalArr = [];
+	for (let i = start; i <= end; i++) {
+		let arrNum = [];
+		for (let j = 0; j < i; j++) {
+			if (i % j == 0) {
+				arrNum.push(j);
+			}
+		}
+		arr.push(arrNum);
+	}
+ // Здесь находим суммы делителей для всех чисел и помещаем в обьект, чтобы 
+ // нумерация совпадала с суммами делителей
+	let arrSum = {};
+	for (let i = 0; i < arr.length; i++) {
+		let ar = 0;
+		for (let j = 0; j < arr[i].length; j++) {
+			ar += arr[i][j];
+		}
+		arrSum[start + i] = ar;
+	}
+	//здесь собственно и находим наши дружественные числа
+	for (let i = start; i <= end; i++) {
+		for (let j = start; j <= end; j++) {
+			if (arrSum[j] == i && arrSum[i] == j && j != i && j > i) {
+				let arrEuler = [];
+				arrEuler.push(i);
+				arrEuler.push(j);
+				if (arrEuler.length == 2) {
+					finalArr.push(arrEuler);
+				}
+			}
+		}
+	}
+	return finalArr;
+}
+getFriendlyNumbers(1, 1210);
