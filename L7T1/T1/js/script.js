e7356e308a97fbe59c3df8254bb348f8ff8d6b40
Paul Lytvyn <36439227+PaulLitvin@users.noreply.github.com>
2018-10-19 09:54:42 +0300

    v1.1

--- L7T1/T1/js/script.js
+++ L7T1/T1/js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
 
     'use strict';
     console.log('1');
@@ -7,26 +7,26 @@ window.addEventListener('DOMContentLoaded', function() {
         tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideTabContent(a) {
-        for (let i = a; i < tabContent.length; i++){
+        for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
     }
 
     hideTabContent(1);
-    
-    function showTabContent(b){
-        if (tabContent[b].classList.contains('hide')){
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
         }
     }
 
-    info.addEventListener('click', function(event) {
+    info.addEventListener('click', function (event) {
         let target = event.target;
-        if(target && target.classList.contains('info-header-tab')){
-            for(let i = 0; i < tab.length; i++){
-                if(target == tab[i]){
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
                     break;
