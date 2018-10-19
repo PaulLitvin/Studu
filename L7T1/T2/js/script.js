5dbea3f4658221e4e35a2cca2d71510d5875d925
Paul Lytvyn <36439227+PaulLitvin@users.noreply.github.com>
2018-10-19 09:56:04 +0300

    v1.1

--- L7T1/T2/js/script.js
+++ L7T1/T2/js/script.js
@@ -1,27 +1,27 @@
 
-    window.addEventListener('DOMContentLoaded', function(){
-        "use strict";
+    window.addEventListener('DOMContentLoaded', function () {
+    "use strict";
 
-        function currentTime(){
-            let time = new Date(),
-                elem = document.querySelector('.time'),
-                hours = addZero(time.getHours()),
-                minutes = addZero(time.getMinutes()),
-                seconds = addZero(time.getSeconds());
+    function currentTime() {
+        let time = new Date(),
+            elem = document.querySelector('.time'),
+            hours = addZero(time.getHours()),
+            minutes = addZero(time.getMinutes()),
+            seconds = addZero(time.getSeconds());
 
-            elem.innerHTML = hours + ":" + minutes + ":" + seconds;
+        elem.innerHTML = hours + ":" + minutes + ":" + seconds;
+    }
+
+    currentTime();
+
+    let intreval = setInterval(currentTime, 1000);
+
+    function addZero(num) {
+        if (num < 10) {
+            num = '0' + num;
         }
-        
-        currentTime();
-
-        let intreval = setInterval(currentTime, 1000);
-
-        function addZero(num){
-            if(num < 10){
-                num = '0' + num;
-            }
-            return num;
-        }
-    });
+        return num;
+    }
+});
         
     
