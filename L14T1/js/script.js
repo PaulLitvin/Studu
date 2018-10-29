1d687011f7730ba2e938892ed6d62217feb04f10
Paul Lytvyn <36439227+PaulLitvin@users.noreply.github.com>
2018-10-29 17:48:15 +0200

    v1.1

--- L14T1/js/script.js
+++ L14T1/js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
-    $('.main_btn, .main_btna, .main_nav nav ul li:eq(1), .close').on('click', function () {
+    $('.main_btn, .main_btna, .main_nav nav ul li:eq(1)').click(function () {
+        $(this).toggleClass('overlay');
+    });
+
+    $('overlay, .close').on('click', function () {
         $('.modal').animate({
             opacity: 'toggle',
             height: 'toggle',
